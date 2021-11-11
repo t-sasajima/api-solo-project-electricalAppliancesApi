298eb2757e4a0709c9823b08446471dc87a89975
Takayuki Sasajima <t-sasajima@nri.co.jp>
2021-11-11 14:15:02 +0900

    setup db

---
+++ migrations/20211111051024_add_comments_table.js
@@ -0,0 +1,27 @@
+exports.up = function(knex, Promise) {
+    // create the 'comments' table
+    return knex.schema.createTable("comments", (t) => {
+      t.increments() // auto-incrementing id column
+        .index(); // index this column
+  
+      t.string("username", 255) // maximum length of 255 characters
+        .notNullable() // add a not-null constraint to this column
+
+      t.text("text") // maximum length of 255 characters
+        .notNullable() // add a not-null constraint to this column 
+
+      t.integer("appliance_id")
+        .unsigned()
+        .references("id")
+        .inTable("appliances");
+
+        t.timestamp("created_at")
+        .notNullable()
+        .defaultTo(knex.fn.now());         
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    // undo this migration by destroying the 'comments' table
+    return knex.schema.dropTable("comments");
+  };
